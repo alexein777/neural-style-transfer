b26c0763bf194350b1b4573825cb083a8d7b1737
Aleksandar <a.mladenovic96@gmail.com>
2021-04-26 01:06:21 +0200

    feat: first model iteration
    Implemented all the neccessary steps to iterate and train the model.
    Code still needs a lot of refactoring.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils import *
+from neural_style_transfer import *
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
@@ -7,17 +8,65 @@ import os
 
 
 if __name__ == '__main__':
-    print()
-    # model = load_vgg_model(Config.VGG_MODEL_PATH)
+    # model['input'].assign(image)
+    # sess.run(model['conv4_2'])  # activate layer
+    # tf.reset_default_graph()
+
+    tf.compat.v1.disable_v2_behavior()
+    tf.compat.v1.disable_eager_execution()
+    tf.compat.v1.reset_default_graph()
+    sess = tf.compat.v1.InteractiveSession()
+
+    content_im = Image.open('./input/01_content_beth_harmon.jpg')
+    content_im_resized = content_im.resize((Config.IMAGE_WIDTH, Config.IMAGE_HEIGHT), Image.BILINEAR)
+    content_image = np.asarray(content_im_resized)
+
+    style_im = Image.open('./input/01_style_chess_fantasy.jpg')
+    style_im_resized = style_im.resize((Config.IMAGE_WIDTH, Config.IMAGE_HEIGHT), Image.BILINEAR)
+    style_image = np.asarray(style_im_resized)
+
+    content_image = reshape_and_normalize_image(content_image)
+    style_image = reshape_and_normalize_image(style_image)
+    generated_image = generate_noise_image(content_image, noise_ratio=0.5)
+
+    # Load VGG
+    model = load_vgg_model(Config.VGG_MODEL_PATH)
+
+    sess.run(model['input'].assign(content_image))
+    out = model['conv4_2']
+    a_content = sess.run(out)
+    a_generated = out
+    content_cost = calculate_content_cost(a_content, a_generated)
     #
-    # # model['input'].assign(image)
-    # # sess.run(model['conv4_2'])  # activate layer
-    # # tf.reset_default_graph()
-    # sess = tf.compat.v1.Session()
-    #
-    # content_image = np.asarray(Image.open('./input/01_content_beth_harmon.jpg'))
-    # style_image = np.asarray(Image.open('./input/01_style_chess_fantasy.jpg'))
-    #
-    # content_image = reshape_and_normalize_image(content_image)
-    # style_image = reshape_and_normalize_image(style_image)
-    # generated_image = generate_noise_image(content_image, noise_ratio=0.7)
+    sess.run(model['input'].assign(style_image))
+    style_cost = calculate_style_cost(sess, model, STYLE_LAYER_WEIGHTS)
+    total_cost = calculate_total_cost(content_cost, style_cost)
+
+    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.1)
+    train_step = optimizer.minimize(total_cost)
+
+    def _model_nn(sess, input_image, num_iter=500):
+        sess.run(tf.compat.v1.global_variables_initializer())
+
+        sess.run(model['input'].assign(input_image))
+
+        for i in range(num_iter):
+            # Minimize total cost
+            sess.run(train_step)
+
+            # Generate image again, as params have changed
+            generated_image = sess.run(model['input'])
+
+            if i % 30 == 0:
+                J_total, J_content, J_style = sess.run([total_cost, content_cost, style_cost])
+                print(f'\nIteration: {i}')
+                print('------------------------------------')
+                print(f'\t* Total cost: {J_total}')
+                print(f'\t* Content cost: {J_content}')
+                print(f'\t* Style cost: {J_style}')
+
+        save_image(generated_image, './output/generated_image.jpg')
+
+        return generated_image
+
+    _model_nn(sess, generated_image)
--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -4,19 +4,19 @@ import tensorflow as tf
 
 
 STYLE_LAYER_WEIGHTS = {
-    'conv1_1': 0.2,
+    'conv1_1': 0.15,
     'conv2_1': 0.2,
-    'conv3_1': 0.2,
+    'conv3_1': 0.3,
     'conv4_1': 0.2,
-    'conv5_1': 0.2
+    'conv5_1': 0.15
 }
 
 
-def content_cost(a_content, a_generated):
+def calculate_content_cost(a_content, a_generated):
     """Calculate content cost from activations of a chosen layer."""
 
     # Extract dimensions from activation tensor (ignore mini-batch dimension)
-    _, n_H, n_W, n_C = a_content.get_shape().as_list()
+    _, n_H, n_W, n_C = a_content.shape
     norm_expr = 1 / (4 * n_H * n_W * n_C)
 
     return norm_expr * tf.reduce_sum(tf.square(tf.subtract(a_content, a_generated)))
@@ -35,11 +35,11 @@ def gram_matrix(a_unrolled):
 def layer_style_cost(a_style, a_generated):
     """Calculate style cost for activations of a specific layer."""
 
-    _, n_H, n_W, n_C = a_style.get_shape().as_list()
+    _, n_H, n_W, n_C = a_style.shape
 
     # Unroll activation tensors into matrices, but change the order of dimensions so that n_C is first
-    a_style_unrolled = tf.reshape(tf.transpose(a_style, perm=[3, 1, 2, 0], shape=[n_C, -1]))
-    a_generated_unrolled = tf.reshape(tf.transpose(a_generated, perm=[3, 1, 2, 0], shape=[n_C, -1]))
+    a_style_unrolled = tf.reshape(tf.transpose(a_style, perm=[3, 1, 2, 0]), shape=[n_C, -1])
+    a_generated_unrolled = tf.reshape(tf.transpose(a_generated, perm=[3, 1, 2, 0]), shape=[n_C, -1])
 
     gram_style = gram_matrix(a_style_unrolled)
     gram_generated = gram_matrix(a_generated_unrolled)
@@ -48,7 +48,7 @@ def layer_style_cost(a_style, a_generated):
     return norm_expr * tf.reduce_sum(tf.square(tf.subtract(gram_style, gram_generated)))
 
 
-def style_cost(sess, model, style_layer_weights):
+def calculate_style_cost(sess, model, style_layer_weights):
     """
     Calculate overall style cost of the model.
     :param model: Dict, keys - names of VGG layers, values - tensors
@@ -69,6 +69,6 @@ def style_cost(sess, model, style_layer_weights):
     return cost
 
 
-def total_cost(content_cost, style_cost, alpha=12, beta=30):
+def calculate_total_cost(content_cost, style_cost, alpha=10, beta=40):
     """Calculate total cost function as linear combination of content and style costs."""
     return alpha * content_cost + beta * style_cost
